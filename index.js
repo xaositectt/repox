9b7f73db58dc79b98aed74b54e0bca931408716b
xaositectt <xaositectt@gmail.com>
2018-12-03 00:00:37 +0100

    added inquirer questions about username and password

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
+const inquirer = require('./lib/inquirer');
 
 clear();
 console.log(chalk.green(figlet.textSync('REPOX', {horizontalLayout: 'full'})));
 
-if (files.dirExists('.git')) {
-  console.log(chalk.red('the git repository already exists'));
-  process.exit();
+const run = async () => {
+  const credentials = await inquirer.askCredentials();
+  console.log(credentials);
 }
+
+run();
+
+// if (files.dirExists('.git')) {
+//   console.log(chalk.red('the git repository already exists'));
+//   process.exit();
+// }
